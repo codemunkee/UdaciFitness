9b50a86852272fb305f2c794a76e29de170f9904
Russ Nealis <rnealis@twilio.com>
2018-02-09 09:00:19 -0800

    added AsyncStorage for storing updates

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciSteppers from './UdaciSteppers';
 import DateHeader from './DateHeader';
 import { Ionicons } from '@expo/vector-icons';
 import TextButton from './TextButton';
+import { submitEntry, removeEntry } from '../utils/api';
 
 function SubmitBtn({ onPress }) {
   return (
@@ -73,6 +74,7 @@ export default class AddEntry extends Component {
     // Navigate to home
 
     // Save to 'DB'
+    submitEntry({ key, entry });
 
     // Clear local notification
   }
@@ -85,6 +87,7 @@ export default class AddEntry extends Component {
     // Rout to Home
 
     // Update "DB"
+    removeEntry(key);
   }
 
   render() {
