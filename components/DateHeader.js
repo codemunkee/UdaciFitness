0627f35766b18936aba0c3a84330a230b05ab71c
Russ Nealis <rnealis@twilio.com>
2018-02-11 08:24:40 -0800

    more styling for logged entries

--- components/DateHeader.js
+++ components/DateHeader.js
@@ -4,7 +4,7 @@ import { purple } from '../utils/colors';
 
 export default function DateHeader({ date }) {
   return (
-    <Text style={{color: purple, fontSize: 25}}>
+    <Text style={{color: purple, fontSize: 20}}>
       {date}
     </Text>
   )
--- components/History.js
+++ components/History.js
@@ -27,16 +27,26 @@ class History extends Component {
   renderItem = ({today, ...metrics}, formattedDate, key) => (
     <View style={styles.item}>
       {today
-        ? <Text>{JSON.stringify(today)}</Text>
-        : <Text>{JSON.stringify(metrics)}</Text>
+        ? <View>
+            <DateHeader date={formattedDate} />
+            <Text style={styles.noDataText}>
+              {today}
+            </Text>
+          </View>
+        : <TouchableOpacity onPress={() => console.log("Pressed!")}>
+            <Text>{JSON.stringify(metrics)}</Text>
+          </TouchableOpacity>
       }
     </View>
   )
 
   renderEmptyDate(formattedDate) {
     return (
-      <View>
-        <Text>No Data for this day</Text>
+      <View style={styles.item}>
+        <DateHeader date={formattedDate} />
+        <Text style={styles.noDataText}>
+          You didnt log any data on this day.
+        </Text>
       </View>
     )
   }
@@ -70,6 +80,11 @@ const styles = StyleSheet.create({
       width: 0,
       height: 3,
     }
+  },
+  noDataText: {
+    fontSize: 20,
+    paddingTop: 20,
+    paddingBottom: 20,
   }
 })
 
