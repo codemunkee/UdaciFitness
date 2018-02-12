1ba8eaeb314ad785f949014ce30569502229d07e
Russ Nealis <rnealis@twilio.com>
2018-02-11 17:05:20 -0800

    status bar

--- App.js
+++ App.js
@@ -8,6 +8,15 @@ import History from './components/History'
 import { TabNavigator } from 'react-navigation';
 import { purple, white } from './utils/colors';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import { Constants } from 'expo';
+
+function UdaciStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 const Tabs = TabNavigator({
   History: {
@@ -49,7 +58,7 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <View style={{height: 20}} />
+          <UdaciStatusBar backgroundColor={purple} barStyle='light-content' />
           <Tabs />
         </View>
       </Provider>
